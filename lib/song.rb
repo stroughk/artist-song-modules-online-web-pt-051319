5c5926863ed476b38ded463e4b1ae40f52071788
Kateryna Strough <katerynastrough@hotmail.com>
2019-06-15 13:52:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,8 @@ require 'pry'
 class Song
   
   extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
   include Paramable::InstanceMethods
   
   attr_accessor :name
