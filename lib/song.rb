d4f59b7d00bb8dee4cfee77c1228a8db24141a2b
Kateryna Strough <katerynastrough@hotmail.com>
2019-06-15 13:52:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,13 +12,6 @@ class Song
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
 
   def self.all
     @@songs
